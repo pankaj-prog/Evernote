fd39b494a11c0c2f30543c25259f4ec237555294
pankaj-prog <82706396+pankaj-prog@users.noreply.github.com>
2021-04-30 15:04:22 +0530

    installed firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {projectFirestore} from './firebase/Config'
 
 function App() {
   return (
---
+++ src/firebase/Config.js
@@ -0,0 +1,26 @@
+//  import * as  firebase from 'firebase'
+// import {app} from 'firebase';
+import firebase from 'firebase/app'
+import 'firebase/firestore';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCECwDYxZwXWb_dvblbpUk-Hc1SaMaWy2A",
+    authDomain: "evernote-ed9ee.firebaseapp.com",
+    projectId: "evernote-ed9ee",
+    storageBucket: "evernote-ed9ee.appspot.com",
+    messagingSenderId: "1001401364647",
+    appId: "1:1001401364647:web:e43db1b3ce46d2fe0d6b82",
+    measurementId: "G-GWNQQH0F0D"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+   
+export  const projectFirestore = firebase.firestore();
+   
+
+
+
+  
+
+  
