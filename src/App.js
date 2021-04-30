76436eb52813cb2fbcb89b772f1808c7c6c90948
pankaj-prog <82706396+pankaj-prog@users.noreply.github.com>
2021-04-30 17:46:59 +0530

    updated app.js
    linked firestore

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import {projectFirestore} from './firebase/Config'
 
-function App() {
-  return (
-    <div >HELLO </div>
-  );
+class App extends React.Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      selectedNoteIndex: null,
+      selectedNote: null,
+      notes: null
+    };
+  }
+
+  render(){
+    return(
+      <div>HELLO WORLD</div>
+    )
+  }
+
+  componentDidMount = () => {
+    projectFirestore
+      .collection('notes')
+      .onSnapshot(serverUpdate => {
+        const notes = serverUpdate.docs.map(_doc => {
+          const data = _doc.data();
+          data['id'] = _doc.id;
+          return data;
+        });
+        console.log(notes);
+        this.setState({ notes: notes });
+      });
+  }
+  
+
 }
 
 export default App;
